73578ebaccb6eff984cd07a6893a42794bd323b4
dhwjdgus <dhwjdgus88@naver.com>
2024-12-04 19:17:04 +0900

    아이템 프리펩, 제너레이터 추가
    전기, 물방울 아이템 프리펩 & 코드 추가
    제너레이터 코드 추가

--- OhMyComputer!/Assets/dropItemController.cs
+++ OhMyComputer!/Assets/dropItemController.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class dropitemcontroller : MonoBehaviour
 {
+    GameObject com;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.com = GameObject.Find("com");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, -0.1f, 0);
+
+        if(transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+
+        Vector2 p1 = transform.position;
+        Vector2 p2 = this.com.transform.position;
+        Vector2 dir = p1 - p2;
+        float d = dir.magnitude;
+        float r1 = 0.5f;
+        float r2 = 1.0f;
+
+        //if (d < r1 + r2)
+        //{
+        //    GameObject director = GameObject.Find("GameDirector");
+        //    director.GetComponent<GameDirector>().DecreaseHp();
+
+        //    Destroy(gameObject);
+        //}
+
     }
 }
--- OhMyComputer!/Assets/dropItemGenerator.cs
+++ OhMyComputer!/Assets/dropItemGenerator.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class dropItemGenerator : MonoBehaviour
 {
+    public GameObject electricPrefab;
+    public GameObject waterPrefab;
+    float span = 1.0f;
+    float delta = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            float randomValue = Random.value; // 0.0f ~ 1.0f »çÀÌÀÇ °ª
+
+            if (randomValue < 0.7f) // 70% È®·ü
+            {
+                GameObject et = Instantiate(electricPrefab);
+                int px = Random.Range(-6, 7);
+                et.transform.position = new Vector3(px, 7, 0);
+            }
+            else // 30% È®·ü
+            {
+                GameObject wt = Instantiate(waterPrefab);
+                int wx = Random.Range(-6, 7);
+                wt.transform.position = new Vector3(wx, 7, 0);
+            }
+        }
     }
 }
