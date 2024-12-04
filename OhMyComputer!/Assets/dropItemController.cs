49e5637ad04fac2fb3a85ce97a2a2b80b835686d
dhwjdgus <dhwjdgus88@naver.com>
2024-12-04 19:33:35 +0900

    아이템 충돌판정
    전기, 물방울 프리펩이 노트북과 충돌판정 할 수 있는 코드 설정

--- OhMyComputer!/Assets/dropItemController.cs
+++ OhMyComputer!/Assets/dropItemController.cs
@@ -29,13 +29,13 @@ public class dropitemcontroller : MonoBehaviour
         float r1 = 0.5f;
         float r2 = 1.0f;
 
-        //if (d < r1 + r2)
-        //{
-        //    GameObject director = GameObject.Find("GameDirector");
-        //    director.GetComponent<GameDirector>().DecreaseHp();
+        if (d < r1 + r2)
+        {
+            GameObject director = GameObject.Find("GameDirector");
+            //director.GetComponent<GameDirector>().DecreaseHp();
 
-        //    Destroy(gameObject);
-        //}
+            Destroy(gameObject);
+        }
 
     }
 }
