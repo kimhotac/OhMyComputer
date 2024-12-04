f4259e5d5891fc18ab435041310e65599df55512
kimhotac <rlaghxkr@Gmail.com>
2024-12-04 19:22:12 +0900

    컴퓨터 컨트롤러 생성
    컴퓨터 화면 밖 나가면 튕겨 나오도록 완성

--- OhMyComputer!/Assets/comController.cs
+++ OhMyComputer!/Assets/comController.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class comController : MonoBehaviour
 {
+    private Camera mainCamera;
+    private Vector2 screenBounds;
+    float speed = 0;
+    Vector2 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 스와이프의 길이를 구한다
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 마우스를 클릭한 좌표
+            this.startPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            // 마우스를 떼었을 때 좌표
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = endPos.x - this.startPos.x;
+
+            // 스와이프 길이를 처음 속도로 변환한다
+            this.speed = swipeLength / 500.0f;
+        }
+
+        transform.Translate(this.speed, 0, 0);  // 이동
+        this.speed *= 0.98f;                    // 감속
+
+        Vector3 objPosition = transform.position;
+
+        if ((objPosition.x > screenBounds.x && this.speed > 0)
+            || (objPosition.x < -screenBounds.x && this.speed < 0))
+        {
+            this.speed =-this.speed;
+        }
     }
 }
