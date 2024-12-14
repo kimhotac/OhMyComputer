76330c6fca258a51648b3066446f4f60e8215426
dhwjdgus <dhwjdgus88@naver.com>
2024-12-14 16:07:46 +0900

    효과음 추가 및 게임 종료씬 수정
    -각 종료 씬마다 효과음 추가(방전 효과음, 축하 효과음)
    -Bad 종료1(물 맞고 종료)씬 전기 동작 추가

--- OhMyComputer!/Assets/ClearBadDIrector.cs
+++ OhMyComputer!/Assets/ClearBadDIrector.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class ClearBadScene : MonoBehaviour
 {
+    GameObject electric2;
+    GameObject electric1;
+    float sign = 1;
+
     int score;
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 60;
+        this.electric1 = GameObject.Find("electric1");
+        this.electric2 = GameObject.Find("electric2");
         this.score = PlayerPrefs.GetInt("Score", 0); // 저장된 점수 불러오기, 기본값은 0
         Debug.Log("Score from previous scene: " + this.score);
+
+      
 
     }
 
@@ -21,5 +34,22 @@ public class ClearBadScene : MonoBehaviour
         {
             SceneManager.LoadScene("GameScene");
         }
+
+
+        //-3.5보다 크면 왼쪽으로 이동 -4.5보다 작으면 오른쪽으로 이동
+        //왼쪽으로 30회 이동 (오른쪽으로 60회 이동 왼쪽으로 60회 이동) 반복
+
+
+        float x = this.electric1.GetComponent<Transform>().transform.position.x;
+        if (x > -3.5f || x < -4.5f)
+        {
+            sign = sign * -1;
+        }
+        this.electric1.GetComponent<Transform>().transform.Translate((sign * 1f / 30), 0, 0);
+        this.electric2.GetComponent<Transform>().transform.Translate((sign * 1f / 30), 0, 0);
+        
+
     }
+
+
 }
